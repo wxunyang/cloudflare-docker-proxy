15a1c4a99293b12a7979498fab2ccc817aec8110
yowang <33387570+wxunyang@users.noreply.github.com>
2024-06-29 11:22:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-import DOCS from './help.html'
- 
-// return docs
-if (url.pathname === "/") {
-  return new Response(DOCS, {
-    status: 200,
-    headers: {
-      "content-type": "text/html"
-    }
-  });
-}
-
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));

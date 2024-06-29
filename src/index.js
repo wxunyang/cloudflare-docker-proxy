b3958a763ab1b371937defe229b0ae9a38517f0c
yowang <33387570+wxunyang@users.noreply.github.com>
2024-06-29 10:42:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.yowang.top": dockerHub,
+  "docker.libcuda.so": dockerHub,
   "quay.libcuda.so": "https://quay.io",
   "gcr.libcuda.so": "https://gcr.io",
   "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
